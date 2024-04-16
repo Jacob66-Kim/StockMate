963c27d4b68f80e5e112010f23ac28feab7cf56f
jacobkim <jacob913.kim@gmail.com>
2024-04-16 12:31:43 +0900

    Add Summary Description to API

--- main.py
+++ main.py
@@ -12,48 +12,48 @@ async def root():
     return {"message": "Hello FastAPI!"}
 
 
-@app.get("/stocks/{ticker}", response_class=JSONResponse)
+@app.get("/stocks/{ticker}", summary="주식 정보 조회", response_class=JSONResponse)
 async def get_stock_info(ticker: str):
     info = await get_info(ticker)
 
     return info
 
 
-@app.get("/stocks_per/{ticker}", response_class=JSONResponse)
+@app.get("/stocks_per/{ticker}", summary="재무제표 PER 값 호출", response_class=JSONResponse)
 async def get_stock_per(ticker: str):
     per = await get_per(ticker)
 
     return per
 
-@app.get("/stocks_price_realtime/", response_class=JSONResponse)
+@app.get("/stocks_price_realtime/", summary="주식 실시간 가격 정보 조회", response_class=JSONResponse)
 async def get_stock_price_realtime(ticker: str):
     price = await get_price_realtime(ticker)
 
     return price
 
 
-@app.get("/stocks_price_range/", response_class=JSONResponse)
+@app.get("/stocks_price_range/", summary="주식 범위 가격 정보 조회", response_class=JSONResponse)
 async def get_stock_price_range(ticker: str, start: str, end: str):
     price = await get_price_range(ticker, start, end)
 
     return price
 
 
-@app.get("/stocks_volume_realtime/", response_class=JSONResponse)
+@app.get("/stocks_volume_realtime/", summary="주식 실시간 거래량 정보 조회", response_class=JSONResponse)
 async def get_stock_volume_realtime(ticker: str):
     volume = await get_volume_realtime(ticker)
 
     return volume.to_dict()
 
 
-@app.get("/stocks_volume_range/", response_class=JSONResponse)
+@app.get("/stocks_volume_range/", summary="주식 범위 거래량 정보 조회", response_class=JSONResponse)
 async def get_stock_volume_range(ticker: str, start: str, end: str):
     volume = await get_volume_range(ticker, start, end)
 
     return volume.to_dict()
 
 
-@app.get("/find_ticker/", response_class=JSONResponse)
+@app.get("/find_ticker/", summary="주식 Ticker 정보 검색용", response_class=JSONResponse)
 async def find_ticker(comname: str):
     ticker = await find_ticker_by_comname(comname)
 
